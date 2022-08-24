002eb9b658f2e5cf3ddff4ea5b75b733956532bb
vikasdongarwar <vikasdongarwar1012@gmail.com>
2022-08-24 20:33:07 +0530

    added expense file

--- expenses.js
+++ expenses.js
@@ -6,6 +6,87 @@ document.getElementById("track").addEventListener("click",()=>{
 
 document.getElementById("team").addEventListener("click",()=>{
     document.getElementById("teammates").style.display="block";
-}) 
+}) ;
     
+let cancel_btn=document.querySelector("#cancel_btn");
+cancel_btn.onclick=()=>{
+    document.getElementById("expensesForm").style.display="none";
+    document.querySelector("#trackExp>#team").style.display="block";
+};
 
+
+let expenses=JSON.parse(localStorage.getItem("expenses")) || [];
+
+window.addEventListener("load",()=>{
+    if(expenses[0]==undefined)
+    {
+        document.querySelector("#addedExp").style.display="none";
+    }
+    else
+    {
+        document.querySelector("#addedExp").style.display="block";
+        displayExpense(expenses);
+    }
+})
+
+let saveExp_btn=document.querySelector("#saveExp_btn");
+saveExp_btn.onclick=()=>{
+    document.querySelector("#addedExp").style.display="block";
+    let expense={
+        date:document.querySelector("#date").value,
+        project:document.querySelector("#chooseProject").value,
+        category:document.querySelector("#category").value,
+        note:document.querySelector("#notes").value,
+        amount:document.querySelector("#amount").value,
+    }
+    expenses.push(expense);
+    localStorage.setItem("expenses",JSON.stringify(expenses))
+    console.log(expenses);
+    displayExpense(expenses);
+};
+
+let displayExpense=(data)=>{
+    console.log(data)
+    let total=0;
+    let cont=document.getElementById("container");
+    cont.innerHTML=null;
+    data.forEach((ele,index)=>{
+        let box=document.createElement("div");
+        box.setAttribute("id","box");
+
+        let date=document.createElement("div");
+        date.innerText=ele.date;
+
+        let div=document.createElement("div");
+            let h4=document.createElement("h4");
+            h4.innerText=ele.project;
+            let p1=document.createElement("p");
+            p1.innerText=ele.category;
+            let p2=document.createElement("p");
+            p2.innerText=ele.note;
+        
+        let exp=document.createElement("h4");
+        exp.innerText=`$ ${ele.amount}`;
+        total=total+Number(ele.amount);
+
+        let b1=document.createElement("button");
+        b1.innerText="Edit";
+        let b2=document.createElement("button");
+        b2.innerText="Delete";
+        b2.onclick=()=>{
+            deleteExpense(ele,index);
+        }
+
+        let hr=document.createElement("hr");
+
+        div.append(h4,p1,p2);
+        box.append(date,div,exp,b1,b2)
+        cont.append(box,hr);
+    });
+    document.querySelector("#total").innerHTML=`Total: <b> $ ${total}</b>`;
+};
+
+let deleteExpense=(ele,index)=>{
+    let expenses=JSON.parse(localStorage.getItem("expenses"));
+    let data=expenses.filter
+}
